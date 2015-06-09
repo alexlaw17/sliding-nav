5627d1aa40594c55df7bb7cd53152f1ce7a72ef4
Alex Law <alexlaw17@gmail.com>
2015-06-09 02:51:55 -0400

    added css & jquery sliding nav-bar

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
-console.log('\'Allo \'Allo!');
+$(function() {
+	var toggle = $('.sliding-navbar-toggle');
+	var main = $('.sliding-navbar-main');
+
+	toggle.click(function() {
+		main.toggleClass('sliding-navbar-active');
+	});
+});
